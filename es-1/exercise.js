b48622e91ff0525a43b800f9b362baf3d0e7a1ac
hawkydawky <ahmetegeyilmaz@gmail.com>
2023-02-01 20:33:16 +0300

    1-5

---
+++ es-1/exercise.js
@@ -0,0 +1,5 @@
+const brand = "Porsche";
+const model = "911 Carrera 4S";
+const productionYear = 2002;
+let ownersName = "John Doe";
+let isNew = false;
---
+++ es-2/exercise.js
@@ -0,0 +1,6 @@
+let firstName = 'Mario';
+let surName = 'Rossi';
+let age = 20;
+let isMarried = false;
+let job = 'developer';
+let isGraduated = false;
